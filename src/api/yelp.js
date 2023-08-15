d77bdc050ab9f14bedffa0b4fc8cde4e9ecf1c0f
Mahmoud Ashraf <99623614+MahmoudAshraf12@users.noreply.github.com>
2023-08-15 14:42:33 +0300

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -5,6 +5,6 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer zFhI8KBs0kvzs1RSokQhddtymSyFmirmaYpXNYCt8MbFB1_EljreARFlj9p815UYBT1KizdaY9ZE498rfY1lKzZLgtpW0fGxl9WOWZYMGkReoewZZWFzk20r8m1vZHYx'
+        Authorization: 'Bearer ${ApiKey}'
     }
 });
